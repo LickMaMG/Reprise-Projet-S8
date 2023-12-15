105cea615b339a068e27cb034c49584efd723a62
malick <malickgning14@gmail.com>
2023-12-16 00:02:41 +0100

    adding line for data splitting

--- src/data/noise_data.py
+++ src/data/noise_data.py
@@ -1,7 +1,8 @@
 import os, cv2, uuid
 import numpy as np
-import matplotlib.pyplot as plt
 from tqdm import tqdm
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 from data.process import GaussianNoise
 
@@ -17,15 +18,14 @@ class NoiseData:
 
         self.stent_dir      = os.path.join(basedir, "stents")
         self.savedir        = os.path.join(basedir, "data-%s" % data_name)
-        self.annot_filename = os.path.join(basedir, "noise-annots-%s.txt" % data_name)
+        self.annot_filename = os.path.join(basedir, "noise-annots-%s" % data_name)
 
         os.makedirs(self.savedir, exist_ok=True)
         self.create_data()
         
 
     def create_data(self) -> None:
-        gaussian_noise = GaussianNoise()
-        annots = ""
+        annots = []
 
         for entry in os.scandir(self.stent_dir):
             stent_name = entry.name
@@ -35,10 +35,16 @@ class NoiseData:
                 noised_filename   = os.path.join(self.savedir, "%s.jpg" % noised_filename)
                 original_filename = os.path.join(self.stent_dir, stent_name)
 
-                noised_stent = gaussian_noise(image=stent, scale=scale)
+                noised_stent = GaussianNoise(image=stent, **{"scale": scale})
                 noised_stent = noised_stent * 255.
                 plt.imsave(noised_filename, noised_stent, cmap="gray")
-                annots += "%s, %s, %s\n" % (noised_filename, original_filename, scale)
-        print("%d noised images are created in %s" % (9*self.num_images, self.savedir))
+                annots.append("%s, %s, %s\n" % (noised_filename, original_filename, scale))
+        print("\n%d noised images are created in %s" % (9*self.num_images, self.savedir))
+        train_annots, val_annots = train_test_split(annots, test_size=0.2, random_state=42)
+        val_annots, test_annots = train_test_split(val_annots, test_size=0.5, random_state=42)
         
-        with open(self.annot_filename, "w") as file: file.write(annots)
+        for name, dataset in [("train", train_annots), ("validation", val_annots), ("test", test_annots)]:
+            print("%s : %d" % (name.ljust(10), len(dataset)))
+            dataset = ''.join(dataset)
+            with open(self.annot_filename+"-%s.txt" % name, 'w') as file:
+                file.write(dataset)
